220fda459507c163fc13fe03b6a907edd38df324
Muhammad Hafizh Auliansyah <t_m.hafizh@agate.id>
2026-09-17 23:27:27 +0700

    feat & chore: implement safe area detection and adjustment css

--- Assets/Plugins/WebGLPlugin.jslib
+++ Assets/Plugins/WebGLPlugin.jslib
@@ -14,5 +14,26 @@ mergeInto(LibraryManager.library, {
 
         // Default to desktop if neither Android nor iOS is detected
         return 0; // Desktop
+    },
+    GetSafeArea: function()
+    {
+        var div = document.createElement('div');
+        div.style.padding = 'env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left)';
+        document.body.appendChild(div);
+
+        var style = window.getComputedStyle(div);
+        var top = parseFloat(style.paddingTop) || 0;
+        var right = parseFloat(style.paddingRight) || 0;
+        var bottom = parseFloat(style.paddingBottom) || 0;
+        var left = parseFloat(style.paddingLeft) || 0;
+
+        document.body.removeChild(div);
+
+        var result = top + "," + right + "," + bottom + "," + left;
+        var bufferSize = lengthBytesUTF8(result) + 1;
+        var buffer = _malloc(bufferSize);
+        stringToUTF8(result, buffer, bufferSize);
+        return buffer;
     }
+
 });
---
+++ Assets/Scripts/UI Manager/WebGLSafeArea.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace Assets.Scripts.UI_Manager
+{
+    [RequireComponent(typeof(RectTransform))]
+    public class WebGLSafeArea : MonoBehaviour
+    {
+        private RectTransform _rectTransform;
+
+        private void Awake()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+        }
+
+        private void Start()
+        {
+            ApplySafeArea();
+        }
+
+        [DllImport("__Internal")]
+        public static extern string GetSafeArea();
+
+        private void ApplySafeArea()
+        {
+#if UNITY_WEBGL && !UNITY_EDITOR
+            string safePadding = GetSafeArea();
+            string[] insets = safePadding.Split(',');
+
+            if(insets.Length == 4)
+            {
+                float top = float.Parse(insets[0]);
+                float right = float.Parse(insets[1]);
+                float bottom = float.Parse(insets[2]);
+                float left = float.Parse(insets[3]);
+
+                _rectTransform.offsetMin = new Vector2(left, bottom);
+                _rectTransform.offsetMax = new Vector2(-right, -top);
+            }
+#endif
+        }
+       
+    }
+}
